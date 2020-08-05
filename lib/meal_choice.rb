f72e45495facb7d9f2c7e1cf1119d3533fb5f28e
Will Gonzalez <will.colocho@gmail.com>
2020-08-05 23:12:14 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,4 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 #def meal_choice(veg1, veg2, protein = tofu)
-  ###end
-meal_choice (carrots, string beans)
+  ####meal_choice (carrots, string beans)
